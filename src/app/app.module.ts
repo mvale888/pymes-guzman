e173ea51132715cf918096f42a5c3ad758398bd6
mvale888 <mvale888@gmail.com>
2021-02-20 23:31:22 -0300

    add menu Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';  
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,9 +13,18 @@ import { MenuComponent } from './components/menu/menu.component';
 import { MockArticulosFamiliasService } from './mock-articulos-familias.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [
+   BrowserModule,
+   FormsModule,
+   RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
+    ])
+
+    ],
   declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [MockArticulosFamiliasService]
+  providers: [MockArticulosFamiliasService, { provide: APP_BASE_HREF, useValue: "/" }]
 })
 export class AppModule { }
